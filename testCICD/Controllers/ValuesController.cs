a3f38176015c72d20c99929bca700153531a8b6d
sandeep reddy <sandeepreddyg77@outlook.com>
2020-08-17 01:05:40 +0530

    values

--- testCICD/Controllers/ValuesController.cs
+++ testCICD/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace testCICD.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "valudfsfe1", "vfdalue2" };
         }
 
         // GET api/values/5
