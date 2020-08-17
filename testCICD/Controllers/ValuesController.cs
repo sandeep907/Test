10631a5b3f0babd07689088cf06bf16062126e55
sandeep reddy <sandeepreddyg77@outlook.com>
2020-08-17 11:01:21 +0530

    sas

--- testCICD/Controllers/ValuesController.cs
+++ testCICD/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace testCICD.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "valudfsfe1", "vfdalue2" };
+            return new string[] { "reddy", "vfdalue2" };
         }
 
         // GET api/values/5
